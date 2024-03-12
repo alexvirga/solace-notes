4bc77ec2842a71f731c1d7bf9894569878bfe64e
alexvirga <49658469+alexvirga@users.noreply.github.com>
2024-03-12 16:39:36 -0700

    default to hosted db

--- server/db.js
+++ server/db.js
@@ -3,13 +3,22 @@ require("dotenv").config();
 
 const isDevelopment = process.env.ENVIRONMENT === "development";
 
+// Uncomment for local db
+
+// const pool = new Pool({
+//   user: process.env.USERNAME,
+//   password: process.env.PASSWORD,
+//   host: process.env.HOST,
+//   port: process.env.PORT,
+//   database: "solace_notes_db",
+//   ssl: !isDevelopment ? { rejectUnauthorized: false } : false,
+// });
+
 const pool = new Pool({
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  port: process.env.PORT,
-  database: "solace_notes_db",
-  ssl: !isDevelopment ? { rejectUnauthorized: false } : false,
+  connectionString: process.env.CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
