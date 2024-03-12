644cb8d37f9d9c4b113a350fd4d9f4642001f319
alexvirga <49658469+alexvirga@users.noreply.github.com>
2024-03-12 15:24:04 -0700

    remove ssl config for local

--- server/db.js
+++ server/db.js
@@ -1,15 +1,15 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+const isDevelopment = process.env.ENVIRONMENT === "development";
+
 const pool = new Pool({
   user: process.env.USERNAME,
   password: process.env.PASSWORD,
   host: process.env.HOST,
   port: process.env.PORT,
   database: "solace_notes_db",
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: !isDevelopment ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
